809778dd2508f06bcc83244d40b579ccb8e8d16b
tayelolu101 <t_whizzy101@yahoo.com>
2017-11-21 11:07:53 +0100

    Add files via upload

---
+++ LoadPropFile.java
@@ -0,0 +1,157 @@
+package com.zenithbank.merchantclient;
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoadPropFile {
+
+	private static LoadPropFile instance = null;
+	private static final Logger logger = LoggerFactory.getLogger(LoadPropFile.class);
+	private String CRON_TIMER = "cronTimer";
+	private String workingDir = System.getProperty("user.dir");
+	private String fileConfigLocation = workingDir+"\\"+"ClientProp.properties";
+	private String cronTimer;
+	private String classLoaderLocation = "com/zenithbank/merchantclient/ClientProp.properties";
+	
+	private String url ;
+	private String server;
+	private String userName;
+	private String password;
+	private String host;
+	private String proxy;
+	private String port;
+
+
+	public LoadPropFile() {
+		
+		
+		ifinstance == null){
+
+
+
+
+
+		Properties prop = new Properties();
+	
+		FileInputStream fileInputStream = null;
+		InputStream inputStream = null;
+		try {
+			fileInputStream = new FileInputStream(fileConfigLocation);
+		} catch (FileNotFoundException e1) {
+			logger.info( "  Please Include file in location ::: " +fileConfigLocation);
+			logger.info(" Using default file bundled :::: " + classLoaderLocation) ;
+		    inputStream = LoadPropFile.class.getClassLoader()
+					.getResourceAsStream(classLoaderLocation);
+			
+			
+		}
+		try {
+			
+			if(fileInputStream == null){
+				
+				prop.load(inputStream);
+			}else{
+				
+
+				prop.load(fileInputStream);
+				logger.info(" Using default file  :::: " +  fileConfigLocation) ;
+			}
+	
+			
+			this.cronTimer = prop.getProperty(CRON_TIMER);
+			this.url  = prop.getProperty("surl");
+			this.server = prop.getProperty("serv_names");
+			this.userName  = prop.getProperty("userName");
+			this.password = prop.getProperty("password");
+			this.host = prop.getProperty("host");
+			this.proxy =  prop.getProperty("proxy");
+			this.port = prop.getProperty("port");
+		
+			System.out.println("Cron timer :: " + this.cronTimer);
+			if(fileInputStream != null){
+				fileInputStream.close();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+
+
+
+
+
+
+	}
+
+	}
+
+	public static LoadPropFile getInstance() {
+		if (instance == null) {
+
+			instance = new LoadPropFile();
+		}
+
+		return instance;
+	}
+
+	public String getCronTimer() {
+		return this.cronTimer;
+	}
+
+	
+
+
+	public String getWorkingDir() {
+		return this.workingDir;
+	}
+
+	
+
+	public String getUrl() {
+		return this.url;
+	}
+
+	
+	public String getServer() {
+		return this.server;
+	}
+
+
+
+	public String getUserName() {
+		return this.userName;
+	}
+
+	
+	public String getPassword() {
+		return this.password;
+	}
+
+
+
+	public String getHost() {
+		return this.host;
+	}
+
+	
+
+	public String getProxy() {
+		return this.proxy;
+	}
+
+	public String getPort() {
+		return this.port;
+	}
+
+	
+
+}
---
+++ MerchantNotificationJob.java
@@ -0,0 +1,313 @@
+package com.zenithbank.merchantclient;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+
+
+
+import org.apache.commons.httpclient.HostConfiguration;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.StatusLine;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MerchantNotificationJob implements Job{
+	private static final Logger logger = LoggerFactory.getLogger(MerchantNotificationJob.class);
+    DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+	private static LoadPropFile propFile = LoadPropFile.getInstance();
+	@Override
+	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+		// TODO Auto-generated method stub
+		logger.info(dateFormat.format(System.currentTimeMillis()) +" ::  Started Main!!!!");
+        MerchantClient merchantClient = new MerchantClient();
+        Connection connection = null;
+        Properties prop = new Properties();
+        InputStream inputStream = merchantClient.getClass()
+		.getClassLoader().getResourceAsStream("com/zenithbank/merchantclient/ClientProp.properties");
+
+        try {
+			prop.load(inputStream);
+		} catch (IOException e) {
+	
+			e.printStackTrace();
+			
+			throw new JobExecutionException("Canot find Input stream for prop file ");
+		}
+
+        try {
+
+            // Properties prop = merchantClient.getProp();
+            String Id;
+            String SessionID;
+            String RequestorID;
+            String PayerPhoneNumber;
+            String PayerName;
+            String MerchantCode;
+            String MerchantName;
+            String MerchantPhoneNumber;
+            String ReferenceCode;
+            Double Amount;
+            String API_HTTP_Method;
+            String API_URL;
+            String API_UserId;
+            String API_Password;
+            String API_Key;
+
+            String urls = propFile.getUrl();
+            String server = propFile.getServer();
+            String userName = propFile.getUserName();
+            String password = propFile.getPassword();
+            String host = propFile.getHost();
+            String proxy = propFile.getProxy();
+            //String domain = prop.getProperty("domain");
+            Integer port = Integer.parseInt(propFile.getPort());
+            
+         /*   logger.info("URLS " + urls);
+            logger.info("server " + server);
+
+            logger.info("userName " + userName);
+
+            logger.info("password " + password);
+
+            logger.info("host " + host);
+
+            logger.info("port " + port);
+
+            logger.info("proxy " + proxy);
+
+            logger.info("URLS " + urls);*/
+
+             
+            Class.forName(server);
+            connection = DriverManager.getConnection(urls);
+
+            String Sqls = "{call zsp_mcash_merchant_notify_ret}";
+            CallableStatement prep = connection.prepareCall(Sqls);
+            ResultSet rs = prep.executeQuery();
+
+            while (rs.next()) {
+
+                Id = rs.getString("Id");
+                SessionID = rs.getString("SessionID");
+                RequestorID = rs.getString("RequestorID");
+                PayerPhoneNumber = rs.getString("PayerPhoneNumber");
+                PayerName = rs.getString("PayerName");
+                MerchantCode = rs.getString("MerchantCode");
+                MerchantName = rs.getString("MerchantName");
+                MerchantPhoneNumber = rs.getString("MerchantPhoneNumber");
+                ReferenceCode = rs.getString("ReferenceCode");
+                Amount = rs.getDouble("Amount");
+                API_HTTP_Method = rs.getString("API_HTTP_Method");
+                API_URL = rs.getString("API_URL");
+                API_UserId = rs.getString("API_UserId");
+                API_Password = rs.getString("API_Password");
+                API_Key = rs.getString("API_Key");
+
+                API_URL = API_URL.replace("{Id}", Id);
+                API_URL = API_URL.replace("{SessionID}", SessionID);
+                API_URL = API_URL.replace("{RequestorID}", RequestorID);
+                API_URL = API_URL.replace("{PayerPhoneNumber}", PayerPhoneNumber);
+                API_URL = API_URL.replace("{PayerName}", PayerName);
+                API_URL = API_URL.replace("{MerchantCode}", MerchantCode);
+                API_URL = API_URL.replace("{MerchantName}", MerchantName);
+                API_URL = API_URL.replace("{MerchantPhoneNumber}", MerchantPhoneNumber);
+                API_URL = API_URL.replace("{ReferenceCode}", ReferenceCode);
+                String Amounts = String.valueOf(Amount);
+                API_URL = API_URL.replace("{Amount}", Amounts);
+                API_URL = API_URL.replace("{API_Key}", API_Key);
+                API_URL = API_URL.replace(" ", "%");
+
+                if (API_UserId == null) {
+                    API_UserId = "";
+                } else {
+                    API_URL = API_URL.replace("{API_UserId}", API_UserId);
+                }
+                if (API_Password == null) {
+                    API_Password = "";
+                } else {
+                    API_URL = API_URL.replace("{API_Password}", API_Password);
+                }
+
+                if (API_HTTP_Method.equalsIgnoreCase("QueryString")) {
+
+                    int length = API_URL.indexOf("?");
+                    String Api = API_URL.substring(0, length);
+                    System.out.println(Api);
+                    System.out.println(API_URL);
+
+                    HttpClient client = new HttpClient();
+                    HttpMethod method = new PostMethod(API_URL);
+                    method.setDoAuthentication(true);
+                    HostConfiguration hostConfig = client.getHostConfiguration();
+                    hostConfig.setHost(host);
+                    hostConfig.setProxy(proxy, port);
+                    //   NTCredentials proxyCredentials = new NTCredentials(userName, password, host, domain);
+                    client.getState().setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(userName, password));
+                    // send the transaction
+                    client.executeMethod(hostConfig, method);
+                    StatusLine status = method.getStatusLine();
+
+                    if (status != null && method.getStatusCode() == 200) {  //success
+
+                        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                        Date dated = new Date();
+                        String notifyDated = dateFormat.format(dated);
+
+                        String query = "UPDATE zib_mcash_merchant_notification SET NotifyMerchant = 1, NotifyMerchatDt = ? WHERE Id = ?";
+                        PreparedStatement preps = connection.prepareCall(query);
+                        preps.setString(1, notifyDated);
+                        preps.setString(2, Id);
+                        preps.executeUpdate();
+
+                        logger.info("UPDATED");
+
+                        logger.info(method.getResponseBodyAsString() + "\n Status code : " + status);
+
+                    } else {
+
+                        System.err.println("Posting Failed : " + method.getStatusCode());
+
+                        String Error = "Posting to this Merchant failed:\n " + method.getStatusLine();
+                        String equery = "UPDATE zib_mcash_merchant_notification SET ErrorText = ?  WHERE Id = ?";
+                        PreparedStatement preps = connection.prepareCall(equery);
+                        preps.setString(1, Error);
+                        preps.setString(2, Id);
+                        preps.executeUpdate();
+                    }
+
+                    method.releaseConnection();
+
+                } else {
+
+                    System.out.println(API_URL);
+
+                    NameValuePair[] data = {
+                            new NameValuePair("Amount", Amounts),
+                            new NameValuePair("Telephone", PayerPhoneNumber),
+                            new NameValuePair("Key", API_Key)};
+
+                     //   String param = "Amount=" + Amounts + "&Telephone=" + PayerPhoneNumber + "& Key=" + API_Key;
+                        /* = "<PaymentNotification>\n"
+                         + "<PayerName>" + PayerName + "</PayerName>\n"
+                         + "<PayerPhoneNumber>" + PayerPhoneNumber + "</PayerPhoneNumber>\n"
+                         + "<ReferenceCode>" + ReferenceCode + "</ReferenceCode>\n"
+                         + "<Amount>" + Amounts + "</Amount>\n"
+                         + "<SessionID>" + SessionID + "</SessionID>\n"
+                         + "<RequestorID>" + RequestorID + "</RequestorID>\n"
+                         + "<MerchantName>" + MerchantName + "</MerchantName>\n"
+                         + "<MerchantPhoneNumber>" + MerchantPhoneNumber + "</MerchantPhoneNumber>\n"
+                         + "<MerchantCode>" + MerchantCode + "</MerchantCode>"
+                         + "</PaymentNotification>";*/
+
+                        HttpClient client = new HttpClient();
+                        PostMethod method = new PostMethod(API_URL);
+                        method.setDoAuthentication(true);
+                        HostConfiguration hostConfig = client.getHostConfiguration();
+                        hostConfig.setHost(host);
+                        hostConfig.setProxy(proxy, port);
+                        //   NTCredentials proxyCredentials = new NTCredentials(userName, password, host, domain);
+                        client.getState().setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(userName, password));
+                        method.setRequestHeader("Content-Type", "application/xml");
+                        method.setRequestHeader("charset", "UTF-8");
+                      //  method.setRequestBody(param);
+                        method.setRequestBody(data);
+                    // send the transaction
+                    client.executeMethod(hostConfig, method);
+
+                    StatusLine status = method.getStatusLine();
+
+                    if (status != null && method.getStatusCode() == 200) {  //success
+
+                    
+                        Date dated = new Date();
+                        String notifyDated = dateFormat.format(dated);
+
+                        String query = "UPDATE zib_mcash_merchant_notification SET NotifyMerchant = 1, NotifyMerchatDt = ? WHERE Id = ?";
+                        PreparedStatement preps = connection.prepareCall(query);
+                        preps.setString(1, notifyDated);
+                        preps.setString(2, Id);
+                        preps.executeUpdate();
+
+                    	logger.info("UPDATED");
+
+                    	logger.info(method.getResponseBodyAsString() + "\n Status code : " + status);
+
+                    } else {
+
+                    	logger.info("Posting Failed : " + method.getStatusCode());
+
+                        String Error = "Posting to this Merchant failed:\n " + method.getStatusLine();
+                        String equery = "UPDATE zib_mcash_merchant_notification SET ErrorText = ?  WHERE Id = ?";
+                        PreparedStatement preps = connection.prepareCall(equery);
+                        preps.setString(1, Error);
+                        preps.setString(2, Id);
+                        preps.executeUpdate();
+                    }
+
+                    method.releaseConnection();
+
+                }
+            }
+
+        } catch (ClassNotFoundException ex) {
+        	logger.info("ERROR :::::: 1");
+     
+        } catch (IOException ex) {
+        
+        	logger.info("ERROR :::::: 2");
+        } catch (SQLException ex) {
+        
+        	logger.info("ERROR :::::: 3");
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+        logger.info(dateFormat.format(System.currentTimeMillis()) +" :: End Main!!!!");
+	}
+	
+	
+	
+
+}
+
+
+
+
+String urlParameters  = "param1=a&param2=b&param3=c";
+byte[] postData       = urlParameters.getBytes( StandardCharsets.UTF_8 );
+int    postDataLength = postData.length;
+String request        = "http://example.com/index.php";
+URL    url            = new URL( request );
+HttpURLConnection conn= (HttpURLConnection) url.openConnection();           
+conn.setDoOutput( true );
+conn.setInstanceFollowRedirects( false );
+conn.setRequestMethod( "POST" );
+conn.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded"); 
+conn.setRequestProperty( "charset", "utf-8");
+conn.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
+conn.setUseCaches( false );
+try( DataOutputStream wr = new DataOutputStream( conn.getOutputStream())) {
+   wr.write( postData );
+}
